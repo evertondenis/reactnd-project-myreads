d3c228751a82ef6b3308ab99c3ccb373345ab45b
Everton Denis <evertondenis@gmail.com>
2017-10-18 22:52:36 -0200

    integrated search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,29 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../../BooksAPI'
+import Book from '../Book'
 
-const Search = () => {
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <Link className='close-search' to='/'>Close</Link>
-        <div className="search-books-input-wrapper">
-          {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class Search extends Component {
 
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-          <input type="text" placeholder="Search by title or author"/>
+  state = {
+    listBooks: []
+  }
 
+  searchBooks(query) {
+    BooksAPI.search(query).then((books) => {
+      console.log(books)
+
+      const listBooks = books
+      this.setState({ listBooks })
+    })
+  }
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className='close-search' to='/'>Close</Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={(event) => this.searchBooks(event.target.value)}
+            />
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.listBooks &&
+              this.state.listBooks.map((book, index) => (
+                <li key={index}>
+                  <Book
+                    image={book.imageLinks.thumbnail}
+                    title={book.title}
+                    author={book.authors}
+                  />
+                </li>
+              ))
+            }
+          </ol>
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid"></ol>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Search
